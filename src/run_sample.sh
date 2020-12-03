e50689c1fb8c79e36484f06c53d9e84dd96b06d8
Naoto Hori <hori.naoto@gmail.com>
2020-12-03 17:22:30 +0000

    Move code to src

---
+++ src/run_sample.sh
@@ -0,0 +1,9 @@
+# To run the program, give arguments as follows:
+
+./a.out -D 0.850 -E 3.975 -d 6.5 -T 0.615 -c 0.005 -f 0.050 -s 1
+
+## Values for -D, -E, -d are fixed parameters in the force field. Do not change.
+## -T is for temperature in kT unit.
+## -c is for concentration of Mg ions.
+## -f is for concentration of monovalent ions.
+## -s is a random seed.
